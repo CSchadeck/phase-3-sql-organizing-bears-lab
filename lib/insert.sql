6be416efe1f68585934b0d759030cd89b6fb1efe
Corey Schadeck <corey@schadeck.com>
2022-06-09 17:40:59 -0400

    commit tree 9e6112ea3f6383fad273f8251f6a6917839203c6

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "feisty", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "aggressive", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "playful", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "funny", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "funny", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "aggressivey", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "playful", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "feisty", 0);
+
+
